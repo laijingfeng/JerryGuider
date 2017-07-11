5c09373a094035a989a67d6b339eef2ef6851c1f
laijingfeng <337292616@qq.com>
2017-07-11 19:45:02 +0800

    预备

---
+++ Assets/Scripts/GameApp.cs
@@ -0,0 +1,40 @@
+﻿using Jerry;
+using UnityEngine;
+
+public class GameApp : MonoBehaviour
+{
+    void Awake()
+    {
+        MyTableLoader.Inst.LoadTables(() =>
+        {
+            GameLogicStart();
+        });
+    }
+
+    void Start()
+    {
+    }
+
+    private void GameLogicStart()
+    {
+        Table.TestA testA = null;
+        if (TestATblMgr.Inst.TryGetValue(10000, out testA))
+        {
+            JerryDebug.Inst.LogInfo(testA, JerryDebug.LogTag.All, true);
+        }
+        else
+        {
+            Debug.LogError("not exist");
+        }
+
+        Table.TestB testB = null;
+        if (TestBTblMgr.Inst.TryGetValue(TestBTblMgr.MakeKey(10000, 10000), out testB))
+        {
+            JerryDebug.Inst.LogInfo(testB, JerryDebug.LogTag.All, true);
+        }
+        else
+        {
+            Debug.LogError("not exist");
+        }
+    }
+}
